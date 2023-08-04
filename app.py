6943f1a34311ebeebd14f83f7abf669b1c4ccfab
Jamshid Azizov <jamshidazizov@Jamshids-iMac.local>
2023-08-04 11:32:20 +0200

    v0.2 - added edit text and remove image/text functionality

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
@@ -60,6 +61,50 @@ def index():
 @app.route('/static/<path:filename>')
 def serve_static(filename):
     return send_from_directory('static', filename)
+
+
+
+# Route for editing an entry (text)
+@app.route('/edit/<int:id>', methods=['POST'])
+def edit_entry(id):
+    conn = connect_db()
+    cur = conn.cursor()
+
+    # Get the new text from the request data
+    data = request.json
+    new_text = data.get('text', '').strip()
+
+    # Update the text in the database
+    cur.execute('UPDATE entries SET description = %s WHERE id = %s', (new_text, id))
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return redirect(url_for('index'))
+
+# Route for deleting an entry (both image and text)
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete_entry(id):
+    conn = connect_db()
+    cur = conn.cursor()
+
+    # Get the image path from the database using the provided ID
+    cur.execute('SELECT image_path FROM entries WHERE id = %s', (id,))
+    image_path = cur.fetchone()[0]
+
+    # Delete the image file from the server
+    if os.path.exists(image_path):
+        os.remove(image_path)
+
+    # Delete the entry from the database
+    cur.execute('DELETE FROM entries WHERE id = %s', (id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
